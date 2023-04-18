41fc27075d51e48202781006ed7b214b96ef90d4
Bodhisattya Mondal <85800324+007Fighter@users.noreply.github.com>
2023-04-18 12:36:37 +0530

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -14,7 +14,6 @@ router.get("/login", (req, res)=>{
 
 
 router.get("/register", async (req, res, next)=>{
-    // console.log(req.body),
     connection.query('SELECT * FROM `rto`', (err, rows) =>{
         if(rows.length > 0){
             res.render("registration",{
@@ -36,10 +35,5 @@ router.get("/helpdesk", (req, res)=>{
 router.get("/welcome", AuthController.isPassengerLoggedIn, (req, res)=>{
     res.render("welcome")
 });
-
-// router.get("/officer_profile", AuthController.isPassengerLoggedIn, (req, res)=>{
-//     res.render("officer_details")
-// });
-
 
 module.exports = router;
